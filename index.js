b870deaeffbf43077c636e35125cafc22bec87e9
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-01-04 14:53:03 +0300

    fix eslint errors

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const http = require('http');
-const net = require('net');
-const url = require('url');
+'use strict';
+
+const http = require(`http`);
+const net = require(`net`);
+const url = require(`url`);
 
 // Create an HTTP tunneling proxy
 const proxy = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('okay');
+  res.writeHead(200, {'Content-Type': `text/plain`});
+  res.end(`okay`);
 });
-proxy.on('connect', (req, cltSocket, head) => {
+proxy.on(`connect`, (req, cltSocket, head) => {
   // connect to an origin server
   const srvUrl = url.parse(`https://${req.url}`);
   const srvSocket = net.connect(srvUrl.port, srvUrl.hostname, () => {
-    cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
-      'Proxy-agent: Node.js-Proxy\r\n' +
-      '\r\n');
+    cltSocket.write(`HTTP/1.1 200 Connection Established\r\n` +
+      `Proxy-agent: Node.js-Proxy\r\n` +
+      `\r\n`);
     srvSocket.write(head);
     srvSocket.pipe(cltSocket);
     cltSocket.pipe(srvSocket);
@@ -23,7 +25,7 @@ proxy.on('connect', (req, cltSocket, head) => {
 const PORT_NUMBER = 1337;
 // now that proxy is running
 proxy.listen(PORT_NUMBER, () => {
-  console.log(`Started proxy @ port ${PORT_NUMBER}`)
+  console.log(`Started proxy @ port ${PORT_NUMBER}`);
 });
 
 
