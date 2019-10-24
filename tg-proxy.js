96cc5c2898db7fb069545b527594c39d8df82e0e
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-10-24 17:21:16 +0300

    fix lint

--- tg-proxy.js
+++ tg-proxy.js
@@ -3,18 +3,24 @@
 const https = require(`https`);
 const {inspect} = require(`util`);
 
-const API_URL = `https://api.telegram.org`;
+const API_URL = `https://httpbin.org`;
 
 const proxy = (req, res) => {
   console.log(`Requesting url`);
-  console.log(inspect(req, {depth: 0}));
+  // console.log(inspect(req, {depth: 0}));
 
   const url = API_URL + req.url;
   console.log(`Requesting url: ${url}`);
 
-  const options = new URL(url);
-  options.method = req.method;
-  options.headers = req.headers;
+  const myUrl = new URL(url);
+  const options = {
+    hostname: myUrl.hostname,
+    port: myUrl.port,
+    path: req.url,
+    method: req.method,
+  };
+  // TODO: filter out correct headers
+  console.log(inspect(options, {depth: 0}));
   const proxyReq = https.request(options, (proxyRes) => {
     const {statusCode} = proxyRes;
     console.log(`Got response: ${statusCode}`);
@@ -22,19 +28,22 @@ const proxy = (req, res) => {
     console.log(`STATUS: ${proxyRes.statusCode}`);
     console.log(`HEADERS: ${JSON.stringify(proxyRes.headers)}`);
 
+    res.statusCode = proxyRes.statusCode;
+    res.headers = proxyRes.headers;
+
     proxyRes.on(`error`, (e) => {
-      console.error(`Proxy res-error: ${e.message}`)
+      console.error(`Proxy res-error: ${e.message}`);
     });
     res.on(`error`, (e) => {
-      console.error(`Res-error: ${e.message}`)
+      console.error(`Res-error: ${e.message}`);
     });
     proxyRes.pipe(res);
 
   });
-  proxyReq.on('error', (e) => {
+  proxyReq.on(`error`, (e) => {
     console.error(`Got proxy-error: ${e.message}`);
   });
-  req.on('error', (e) => {
+  req.on(`error`, (e) => {
     console.error(`Got req-error: ${e.message}`);
   });
 
